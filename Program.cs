5d6fe40ddc491cec27b25f307668c29987009e79
Tony Dayton <code77@Tonys-MacBook-Air.local>
2025-05-02 11:19:24 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my first C# console app!");
+
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("How old are you? ");
+        string age = Console.ReadLine();
+
+        Console.WriteLine($"\nNice to meet you, {name}! You are {age} years old.");
+    }
+}
